eeba7a12a9cad6426bef48417ca28ba723733c6e
GHOST-Aram <owinoojala@gmail.com>
2022-12-05 11:32:52 +0300

    feat: Add books to table by user input:
        -Create method AddToTable:-create tr and td elements
         for every textnode in the table
        -get user input
        -add event listener to form
        -remove table rows in index.html

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
+function addToTable(index,title,author,isbn,dtype){
+    const tableRow = document.createElement('tr')
+
+    const count = document.createElement('th')
+    count.textContent = index
+    tableRow.appendChild(count)
+
+    const tdTitle = document.createElement('td')
+    tdTitle.textContent = title.value
+    tableRow.appendChild(tdTitle)
+
+    const tdAauthor = document.createElement('td')
+    tdAauthor.textContent = author.value
+    tableRow.appendChild(tdAauthor)
+
+    const isbntd = document.createElement('td')
+    isbntd.textContent = isbn.value
+    tableRow.appendChild(isbntd)
+
+    const type = document.createElement('td')
+    type.textContent = dtype.value
+    tableRow.appendChild(type)
+
+    return tableRow
+
+}
+
 const form =  document.querySelector('form')
+const bookTitle = document.querySelector('#book-title')
+const author = document.querySelector('#author-name')
+const ISBN = document.querySelector('#isbn')
+const documentType = document.querySelector('select')
+const table = document.querySelector('tbody')
 
+console.log(form)
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault()
+let index = '#'
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    alert('submitting')
+    const row = addToTable(index,bookTitle,author,ISBN,documentType)
+    table.appendChild(row)
 })
