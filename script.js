e8aa13ea0619efd404a132a91e70c89c3338ffa4
GHOST-Aram <owinoojala@gmail.com>
2022-12-05 13:49:55 +0300

    feat: Store to local Storage
         -Create updateBooks():- add book to books array
                               - store books array in local storage
         -remove addToBooks():

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let books = []
-
-function addToBooks(book){
-    books.push(book)
-}
 
 function createRow(book, index){
     const tableRow = document.createElement('tr')
@@ -44,12 +39,14 @@ function getBookProperties(){
 }
 
 function renderBooks(books){
-    const table = document.querySelector('tbody')
+
+    const tbody = document.querySelector('tbody')
+    tbody.innerHTML = ''
     let index = 1
     books.forEach(book => {
        let row = createRow(book, index)
        index++ 
-       table.appendChild(row)
+       tbody.appendChild(row)
     });
 
     
@@ -59,21 +56,31 @@ function renderRow(row){
     table.appendChild(row)
 }
 
+function updateBooks(book){
+    books.push(book)
+    const local = JSON.stringify(books)
+    window.localStorage.setItem('local',local)
+}
+// Listing Begins
+let books = []
+window.addEventListener('load',(e)=>{
+    let local = JSON.parse(window.localStorage.getItem('local')) 
+    if(Array.isArray(local)){//Chck books in local storage
+        books = local
+        renderBooks(books)   
+    }
+    else{//Create local storage
+        local = JSON.stringify(books)
+        window.localStorage.setItem('local', local)
+    }
+
+})
 const form =  document.querySelector('form')
-// const bookTitle = document.querySelector('#book-title')
-// const author = document.querySelector('#author-name')
-// const ISBN = document.querySelector('#isbn')
-// const documentType = document.querySelector('select')
-
-console.log(form)
-
-
 form.addEventListener('submit', (event)=>{
     event.preventDefault()
     const book = getBookProperties()
-    addToBooks(book)
+    updateBooks(book)
     let index = books.indexOf(book) + 1
     const row = createRow(book, index)
     renderRow(row)
-
 })
